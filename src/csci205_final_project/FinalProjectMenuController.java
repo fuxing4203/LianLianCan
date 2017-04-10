7f2079d53f11519b2f487390625a254985053046
Haipu Sun <hs031@bucknell.edu>
2017-04-09 21:03:40 -0400

    haipu fxml

---
+++ src/csci205_final_project/FinalProjectMenuController.java
@@ -0,0 +1,82 @@
+/* *****************************************
+* CSCI205 - Software Engineering and Design
+* Spring 2017
+*
+* Name: Iris Fu, Haipu Sun, Junjie Jiang, Zilin Ma
+* Date: Apr 9, 2017
+* Time: 8:59:49 PM
+*
+* Project: csci205_final_project
+* Package: csci205_final_project
+* File: FinalProjectMenuController
+* Description:
+*
+* ****************************************
+ */
+package csci205_final_project;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+
+/**
+ * FXML Controller class
+ *
+ * @author hs031
+ */
+public class FinalProjectMenuController implements Initializable {
+
+    @FXML
+    private Button btnStart;
+    @FXML
+    private Button btnLoad;
+    @FXML
+    private Button btnTutorial;
+    @FXML
+    private Button btnOption;
+    @FXML
+    private Button btnRecords;
+    @FXML
+    private Button btnExit;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }
+
+    @FXML
+    void startNewGame() {
+
+    }
+
+    @FXML
+    void loadGame() {
+
+    }
+
+    @FXML
+    void openTutorial() {
+
+    }
+
+    @FXML
+    void openOptions() {
+
+    }
+
+    @FXML
+    void openRecords() {
+
+    }
+
+    @FXML
+    void Exit() {
+
+    }
+
+}
