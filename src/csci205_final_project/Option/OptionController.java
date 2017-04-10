ad3bf3cc4e55d338cc5933ea108d0298379f74d2
Iris Fu <xf002@bucknell.edu>
2017-04-09 21:33:26 -0400

    option fxml created

---
+++ src/csci205_final_project/Option/OptionController.java
@@ -0,0 +1,51 @@
+/* *****************************************
+* CSCI205 - Software Engineering and Design
+* Spring 2017
+*
+* Name: Iris Fu, Haipu Sun, Junjie Jiang, Zilin Ma
+* Date: Apr 9, 2017
+* Time: 9:31:13 PM
+*
+* Project: csci205_final_project
+* Package: csci205_final_project.Option
+* File: OptionController
+* Description:
+*
+* ****************************************
+ */
+package csci205_final_project.Option;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Slider;
+import javafx.scene.control.TitledPane;
+
+/**
+ * FXML Controller class
+ *
+ * @author Iris
+ */
+public class OptionController implements Initializable {
+
+    @FXML
+    private TitledPane optionPane;
+    @FXML
+    private Slider volumeSlider;
+    @FXML
+    private ComboBox<?> dropdownComboBox;
+    @FXML
+    private Button returnBtn;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }
+
+}
