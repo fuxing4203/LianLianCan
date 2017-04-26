c9c87d6c7673b34e947b09d5850a338777cdb433
Haipu Sun <hs031@bucknell.edu>
2017-04-25 23:21:05 -0400

    haipu start image

---
+++ src/csci205_final_project/Records/RecordsController.java
@@ -0,0 +1,46 @@
+/* *****************************************
+* CSCI205 - Software Engineering and Design
+* Spring 2017
+*
+* Name: Iris Fu, Haipu Sun, Junjie Jiang, Zilin Ma
+* Date: Apr 25, 2017
+* Time: 11:19:03 PM
+*
+* Project: csci205_final_project
+* Package: Records
+* File: RecordsController
+* Description:
+*
+* ****************************************
+ */
+package Records;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
+/**
+ * FXML Controller class
+ *
+ * @author hs031
+ */
+public class RecordsController implements Initializable {
+
+    @FXML
+    private Label first;
+    @FXML
+    private Label second;
+    @FXML
+    private Label third;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }
+
+}
