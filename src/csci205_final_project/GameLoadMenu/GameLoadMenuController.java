b4b68bf3f2f2f11178ca91003d148a69327204db
Junjie Jiang <jj030@bucknell.edu>
2017-04-27 23:10:11 -0400

    change

--- src/csci205_final_project/GameLoadMenu/GameLoadMenuController.java
+++ src/csci205_final_project/GameLoadMenu/GameLoadMenuController.java
@@ -19,12 +19,9 @@ import Util.SaveAndLoadModelUtil;
 import csci205_final_project.Game.FinalProjectGameSceneController;
 import csci205_final_project.Model.Level;
 import csci205_final_project.Model.Model;
-import csci205_final_project.Model.Tile;
-import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.URL;
-import java.util.ArrayList;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -33,12 +30,8 @@ import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.image.Image;
-import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.TilePane;
-import javafx.scene.paint.ImagePattern;
-import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
 /**
@@ -133,10 +126,7 @@ public class GameLoadMenuController implements Initializable {
         gameController.setTheModel(save1);
         TilePane tilePane = gameController.getTilePane();
         Level level = gameController.getLevel();
-        setupTilePane(tilePane, level);
-        drawTiles(level, gameController, tilePane);
-
-        setupLables(gameController);
+        gameController.startGameBoardWithMode(level);
         Scene scene = new Scene(game);
         scene.getStylesheets().add("/csci205_final_project/Menu/menu.css");
         stage.setScene(scene);
@@ -164,10 +154,7 @@ public class GameLoadMenuController implements Initializable {
         gameController.setTheModel(save2);
         TilePane tilePane = gameController.getTilePane();
         Level level = gameController.getLevel();
-        setupTilePane(tilePane, level);
-        drawTiles(level, gameController, tilePane);
-
-        setupLables(gameController);
+        gameController.startGameBoardWithMode(level);
         Scene scene = new Scene(game);
         scene.getStylesheets().add("/csci205_final_project/Menu/menu.css");
         stage.setScene(scene);
@@ -195,57 +182,11 @@ public class GameLoadMenuController implements Initializable {
         gameController.setTheModel(save3);
         TilePane tilePane = gameController.getTilePane();
         Level level = gameController.getLevel();
-        setupTilePane(tilePane, level);
-        drawTiles(level, gameController, tilePane);
-
-        setupLables(gameController);
+        gameController.startGameBoardWithMode(level);
         Scene scene = new Scene(game);
         scene.getStylesheets().add("/csci205_final_project/Menu/menu.css");
         stage.setScene(scene);
         stage.show();
-    }
-
-    public void setupLables(FinalProjectGameSceneController gameController) {
-        gameController.getLabelHint().setText(String.valueOf(
-                gameController.getTheModel().getHintChance()));
-        gameController.getLabelShuffle().setText(String.valueOf(
-                gameController.getTheModel().getShuffleChance()));
-        gameController.getLabelScore().setText(String.valueOf(
-                gameController.getTheModel().getScore()));
-    }
-
-    public void setupTilePane(TilePane tilePane, Level level) {
-        tilePane.setPrefColumns(level.getWidth());
-        tilePane.setPrefRows(level.getWidth());
-        tilePane.setPrefWidth(50 * level.getWidth());
-        tilePane.setPrefHeight(50 * level.getHeight());
-        tilePane.setMaxSize(50 * level.getWidth(), 50 * level.getHeight());
-    }
-
-    public void drawTiles(Level level,
-                          FinalProjectGameSceneController gameController,
-                          TilePane tilePane) {
-        for (int i = 1; i < level.getHeight() + 1; i++) {
-            ArrayList<Rectangle> row = new ArrayList();
-            for (int j = 1; j < level.getWidth() + 1; j++) {
-                Tile aTile = gameController.getTheModel().getData().get(i).get(j);
-                Rectangle aRectangle = new Rectangle(50, 50);
-                aRectangle.setOnMouseClicked((MouseEvent eventB) -> {
-                    gameController.selectRectangle(aRectangle, aTile);
-                });
-                if (aTile != null) {
-                    File file = new File(aTile.getImgName());
-                    Image img = new Image(file.toURI().toString());
-                    aRectangle.setFill(new ImagePattern(img));
-                }
-                else {
-                    aRectangle.setOpacity(0);
-                }
-                row.add(aRectangle);
-                tilePane.getChildren().add(aRectangle);
-            }
-            gameController.getData().add(row);
-        }
     }
 
 }
